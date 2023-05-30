f99269bcb2f131e94bafdd5535e6d5c81195cce6
smjk <smjkzsl@gmail.com>
2023-05-30 21:49:43 +0800

    ...

--- apps/system_admin/views/admin/menus.vue
+++ apps/system_admin/views/admin/menus.vue
@@ -19,17 +19,17 @@
 <script>
 // import applist_page from './pages/apps.vue'
 import VueRouter from "/public/libs/vue-router@4.2.1/dist/vue-router.global.js"
-import {system} from './api/api.js'
-import {ref} from 'vue'
+import { system } from './api/api.js'
+import { ref } from 'vue'
 
 
- 
+
 import { Menu as IconMenu, Message, Setting } from 'element_icon'
 
 const router = VueRouter.createRouter({
-        history: VueRouter.createWebHashHistory(),
-        routes: [],
-      })
+  history: VueRouter.createWebHashHistory(),
+  routes: [],
+})
 app.use(router)
 
 
@@ -44,17 +44,17 @@ export default {
     console.log('menus mounted')
     this.getPages()
   },
-  methods:{
-    async getPages(){
-      let data = await system.getPagesList() 
+  methods: {
+    async getPages() {
+      let data = await system.getPagesList()
       console.log(data)
       let _routes = []
-      for(var item in data){
+      for (var item in data) {
         let _name = item
         let url = data[_name]
         const menuItem = {
           name: _name,
-          path: '/' + _name ,
+          path: '/' + _name,
           // icon: route.meta.icon,
           label: _name,
           // 设置 component 属性为一个函数，该函数会动态地加载路由对应的组件
@@ -63,18 +63,18 @@ export default {
         router.addRoute(menuItem)
         router.replace(router.currentRoute.value.fullPath)
 
-        
-         
-      } 
-      debugger
+
+
+      }
+
       this.routes = router.getRoutes()
       console.log(router.getRoutes())
     },
   },
   setup() {
-    
+
     const routes = ref(router.options.routes)
-    return { 
+    return {
       routes
     }
 
