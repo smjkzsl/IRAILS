f59a0d45749ddb3e13bf974237e6a92df38d2396
smjk <smjkzsl@gmail.com>
2023-05-30 21:49:51 +0800

    .

--- apps/system_admin/views/admin/api/api.js
+++ apps/system_admin/views/admin/api/api.js
@@ -1,6 +1,14 @@
-    async function request(url) {
+    async function request(url, options = {}) {
+        const defaultOptions = {
+            method: 'GET',
+            body: null,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const opts = {...defaultOptions, ...options }
         try {
-            const response = await fetch(url)
+            const response = await fetch(url, opts)
             const data = await response.json()
 
             return data
@@ -12,17 +20,20 @@
     }
     const api = {
         user: {
+            path: '/system_admin/user',
             async getCurrentUser() {
-                return await request('/system_admin/user/current_user')
+
+                return await request(`${this.path}/current_user`)
             }
         },
         system: {
+            path: '/system_admin/admin',
             async getAppList() {
-                let data = await request('/system_admin/admin/app_list')
+                let data = await request(`${this.path}/app_list`)
                 return data
             },
             async getPagesList() {
-                let data = await request('/system_admin/admin/pages_list')
+                let data = await request(`${this.path}/pages_list`)
                 return data
             }
         }
