addf56d3657c5b6f99d1433d534c162f387b780b
bruce chou <smjkzsl@gmail.com>
2023-12-23 18:51:08 +0800

    fixed vercel run mode

--- irails/scripts/tpls/project/main.py
+++ irails/scripts/tpls/project/main.py
@@ -4,4 +4,6 @@ if __name__=='__main__':
     
     irails.core.run_server()
     
- 
+else:
+    #for vercel like deploy
+    app = irails.core.generate_mvc_app()
--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import irails
 if __name__=='__main__': 
     
     irails.core.run_server()
+else:
+    app = irails.core.generate_mvc_app()
     
  
